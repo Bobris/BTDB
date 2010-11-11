461671b1e5b018d30180cf8fa5627042d0c1b013
Bobris <Boris.Letocha@gmail.com>
2010-11-11 21:04:55 +0100

    Make some magic numbers a constant.

--- BTDB/Implementation/DefaultTweaks.cs
+++ BTDB/Implementation/DefaultTweaks.cs
@@ -6,46 +6,49 @@ namespace BTDB
 {
     class DefaultTweaks : ITweaks
     {
+        const int OptimumBTreeParentSize = 4096;
+        const int OptimumBTreeChildSize = 4096;
+
         public bool ShouldSplitBTreeChild(int oldSize, int addSize, int oldKeys)
         {
-            return oldSize + addSize > 4096;
+            return oldSize + addSize > OptimumBTreeChildSize;
         }
 
         public bool ShouldSplitBTreeParent(int oldSize, int addSize, int oldChildren)
         {
-            return oldSize + addSize > 4096;
+            return oldSize + addSize > OptimumBTreeParentSize;
         }
 
         public ShouldMergeResult ShouldMergeBTreeParent(int lenPrevious, int lenCurrent, int lenNext)
         {
             if (lenPrevious<0)
             {
-                return lenCurrent + lenNext < 4096 ? ShouldMergeResult.MergeWithNext : ShouldMergeResult.NoMerge;
+                return lenCurrent + lenNext < OptimumBTreeParentSize ? ShouldMergeResult.MergeWithNext : ShouldMergeResult.NoMerge;
             }
             if (lenNext<0)
             {
-                return lenCurrent + lenPrevious < 4096 ? ShouldMergeResult.MergeWithPrevious : ShouldMergeResult.NoMerge;
+                return lenCurrent + lenPrevious < OptimumBTreeParentSize ? ShouldMergeResult.MergeWithPrevious : ShouldMergeResult.NoMerge;
             }
             if (lenPrevious<lenNext)
             {
-                if (lenCurrent + lenPrevious < 4096) return ShouldMergeResult.MergeWithPrevious;
+                if (lenCurrent + lenPrevious < OptimumBTreeParentSize) return ShouldMergeResult.MergeWithPrevious;
             }
             else
             {
-                if (lenCurrent + lenNext < 4096) return ShouldMergeResult.MergeWithNext;
+                if (lenCurrent + lenNext < OptimumBTreeParentSize) return ShouldMergeResult.MergeWithNext;
             }
             return ShouldMergeResult.NoMerge;
         }
 
         public bool ShouldMerge2BTreeChild(int leftCount, int leftLength, int rightCount, int rightLength)
         {
-            if (leftLength + rightLength - 1 > 4096) return false;
+            if (leftLength + rightLength - 1 > OptimumBTreeChildSize) return false;
             return true;
         }
 
         public bool ShouldMerge2BTreeParent(int leftCount, int leftLength, int rightCount, int rightLength, int keyStorageLength)
         {
-            if (leftLength + rightLength - 1 + keyStorageLength > 4096) return false;
+            if (leftLength + rightLength - 1 + keyStorageLength > OptimumBTreeParentSize) return false;
             return true;
         }
 
