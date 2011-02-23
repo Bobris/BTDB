96db58d3192360efb274a58af9e0b75dcfa0ff29
Bobris <Boris.Letocha@gmail.com>
2011-02-23 23:58:08 +0100

    Basic Query provider which just using LINQ for Objects

--- BTDB/ODBLayer/QueryProvider.cs
+++ BTDB/ODBLayer/QueryProvider.cs
@@ -3,6 +3,7 @@
 // This source code is made available under the terms of the Microsoft Public License (MS-PL)
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -43,9 +44,19 @@ namespace BTDB.ODBLayer
 
         public object Execute(Expression expression)
         {
-            // TODO
-            return null;
+            var e = new MakeRunnableExpressionVisitor().Translate(expression);
+            var @delegate = Expression.Lambda(e).Compile();
+            return @delegate.DynamicInvoke();
         }
 
+        public IEnumerable<T> GetEnumerable<T>()
+        {
+            yield break;
+        }
+
+        public IQueryable<T> GetEnumerableAsQueryable<T>()
+        {
+            return GetEnumerable<T>().AsQueryable();
+        }
     }
 }
