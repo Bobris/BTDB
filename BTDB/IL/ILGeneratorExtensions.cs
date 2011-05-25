ceef61d43736f56df6180d5d95be9d6127032b16
Bobris <Boris.Letocha@gmail.com>
2011-05-25 23:54:02 +0200

    It is good enough to have it public.

--- BTDB/IL/ILGeneratorExtensions.cs
+++ BTDB/IL/ILGeneratorExtensions.cs
@@ -5,9 +5,9 @@ using System.Reflection.Emit;
 
 namespace BTDB.IL
 {
-    internal static class ILGeneratorExtensions
+    public static class ILGeneratorExtensions
     {
-        internal static ILGenerator LdcI4(this ILGenerator il, int value)
+        public static ILGenerator LdcI4(this ILGenerator il, int value)
         {
             switch (value)
             {
@@ -51,7 +51,7 @@ namespace BTDB.IL
             return il;
         }
 
-        internal static ILGenerator Ldarg(this ILGenerator il, ushort parameterIndex)
+        public static ILGenerator Ldarg(this ILGenerator il, ushort parameterIndex)
         {
             switch (parameterIndex)
             {
@@ -77,25 +77,25 @@ namespace BTDB.IL
             return il;
         }
 
-        internal static ILGenerator Ldfld(this ILGenerator il, FieldInfo fieldInfo)
+        public static ILGenerator Ldfld(this ILGenerator il, FieldInfo fieldInfo)
         {
             il.Emit(OpCodes.Ldfld, fieldInfo);
             return il;
         }
 
-        internal static ILGenerator Ldflda(this ILGenerator il, FieldInfo fieldInfo)
+        public static ILGenerator Ldflda(this ILGenerator il, FieldInfo fieldInfo)
         {
             il.Emit(OpCodes.Ldflda, fieldInfo);
             return il;
         }
 
-        internal static ILGenerator Stfld(this ILGenerator il, FieldInfo fieldInfo)
+        public static ILGenerator Stfld(this ILGenerator il, FieldInfo fieldInfo)
         {
             il.Emit(OpCodes.Stfld, fieldInfo);
             return il;
         }
 
-        internal static ILGenerator Stloc(this ILGenerator il, ushort localVariableIndex)
+        public static ILGenerator Stloc(this ILGenerator il, ushort localVariableIndex)
         {
             switch (localVariableIndex)
             {
@@ -123,13 +123,13 @@ namespace BTDB.IL
             return il;
         }
 
-        internal static ILGenerator Stloc(this ILGenerator il, LocalBuilder localBuilder)
+        public static ILGenerator Stloc(this ILGenerator il, LocalBuilder localBuilder)
         {
             il.Emit(OpCodes.Stloc, localBuilder);
             return il;
         }
 
-        internal static ILGenerator Ldloc(this ILGenerator il, ushort localVariableIndex)
+        public static ILGenerator Ldloc(this ILGenerator il, ushort localVariableIndex)
         {
             switch (localVariableIndex)
             {
@@ -157,86 +157,86 @@ namespace BTDB.IL
             return il;
         }
 
-        internal static ILGenerator Ldloc(this ILGenerator il, LocalBuilder localBuilder)
+        public static ILGenerator Ldloc(this ILGenerator il, LocalBuilder localBuilder)
         {
             il.Emit(OpCodes.Ldloc, localBuilder);
             return il;
         }
 
-        internal static ILGenerator Ldloca(this ILGenerator il, LocalBuilder localBuilder)
+        public static ILGenerator Ldloca(this ILGenerator il, LocalBuilder localBuilder)
         {
             il.Emit(OpCodes.Ldloca, localBuilder);
             return il;
         }
 
-        internal static ILGenerator Brfalse(this ILGenerator il, Label targetLabel)
+        public static ILGenerator Brfalse(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Brfalse, targetLabel);
             return il;
         }
 
-        internal static ILGenerator BrfalseS(this ILGenerator il, Label targetLabel)
+        public static ILGenerator BrfalseS(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Brfalse_S, targetLabel);
             return il;
         }
 
-        internal static ILGenerator Brtrue(this ILGenerator il, Label targetLabel)
+        public static ILGenerator Brtrue(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Brtrue, targetLabel);
             return il;
         }
 
-        internal static ILGenerator BrtrueS(this ILGenerator il, Label targetLabel)
+        public static ILGenerator BrtrueS(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Brtrue_S, targetLabel);
             return il;
         }
 
-        internal static ILGenerator BrS(this ILGenerator il, Label targetLabel)
+        public static ILGenerator BrS(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Br_S, targetLabel);
             return il;
         }
 
-        internal static ILGenerator BneUnS(this ILGenerator il, Label targetLabel)
+        public static ILGenerator BneUnS(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Bne_Un_S, targetLabel);
             return il;
         }
 
-        internal static ILGenerator BeqS(this ILGenerator il, Label targetLabel)
+        public static ILGenerator BeqS(this ILGenerator il, Label targetLabel)
         {
             il.Emit(OpCodes.Beq_S, targetLabel);
             return il;
         }
 
-        internal static ILGenerator Newobj(this ILGenerator il, Expression<Action> expression)
+        public static ILGenerator Newobj(this ILGenerator il, Expression<Action> expression)
         {
             var constructorInfo = (expression.Body as NewExpression).Constructor;
             return Newobj(il, constructorInfo);
         }
 
-        internal static ILGenerator Newobj(this ILGenerator il, ConstructorInfo constructorInfo)
+        public static ILGenerator Newobj(this ILGenerator il, ConstructorInfo constructorInfo)
         {
             il.Emit(OpCodes.Newobj, constructorInfo);
             return il;
         }
 
-        internal static ILGenerator Callvirt(this ILGenerator il, Expression<Action> expression)
+        public static ILGenerator Callvirt(this ILGenerator il, Expression<Action> expression)
         {
             var methodInfo = (expression.Body as MethodCallExpression).Method;
             return Callvirt(il, methodInfo);
         }
 
-        internal static ILGenerator Callvirt(this ILGenerator il, MethodInfo methodInfo)
+        public static ILGenerator Callvirt(this ILGenerator il, MethodInfo methodInfo)
         {
             if (methodInfo.IsStatic) throw new ArgumentException("Method in Callvirt cannot be static");
             il.Emit(OpCodes.Callvirt, methodInfo);
             return il;
         }
 
-        internal static ILGenerator Call(this ILGenerator il, Expression<Action> expression)
+        public static ILGenerator Call(this ILGenerator il, Expression<Action> expression)
         {
             var newExpression = expression.Body as NewExpression;
             if (newExpression!=null)
@@ -251,115 +251,115 @@ namespace BTDB.IL
             return il;
         }
 
-        internal static ILGenerator Call(this ILGenerator il, MethodInfo methodInfo)
+        public static ILGenerator Call(this ILGenerator il, MethodInfo methodInfo)
         {
             il.Emit(OpCodes.Call, methodInfo);
             return il;
         }
 
-        internal static ILGenerator Call(this ILGenerator il, ConstructorInfo constructorInfo)
+        public static ILGenerator Call(this ILGenerator il, ConstructorInfo constructorInfo)
         {
             il.Emit(OpCodes.Call, constructorInfo);
             return il;
         }
 
-        internal static ILGenerator Mark(this ILGenerator il, Label label)
+        public static ILGenerator Mark(this ILGenerator il, Label label)
         {
             il.MarkLabel(label);
             return il;
         }
 
-        internal static ILGenerator Ldstr(this ILGenerator il, string str)
+        public static ILGenerator Ldstr(this ILGenerator il, string str)
         {
             il.Emit(OpCodes.Ldstr, str);
             return il;
         }
 
-        internal static ILGenerator Throw(this ILGenerator il)
+        public static ILGenerator Throw(this ILGenerator il)
         {
             il.Emit(OpCodes.Throw);
             return il;
         }
 
-        internal static ILGenerator Ret(this ILGenerator il)
+        public static ILGenerator Ret(this ILGenerator il)
         {
             il.Emit(OpCodes.Ret);
             return il;
         }
 
-        internal static ILGenerator Castclass(this ILGenerator il, Type toType)
+        public static ILGenerator Castclass(this ILGenerator il, Type toType)
         {
             il.Emit(OpCodes.Castclass, toType);
             return il;
         }
 
-        internal static ILGenerator Isinst(this ILGenerator il, Type asType)
+        public static ILGenerator Isinst(this ILGenerator il, Type asType)
         {
             il.Emit(OpCodes.Isinst, asType);
             return il;
         }
 
-        internal static ILGenerator ConvU1(this ILGenerator il)
+        public static ILGenerator ConvU1(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_U1);
             return il;
         }
 
-        internal static ILGenerator ConvU2(this ILGenerator il)
+        public static ILGenerator ConvU2(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_U2);
             return il;
         }
 
-        internal static ILGenerator ConvU4(this ILGenerator il)
+        public static ILGenerator ConvU4(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_U4);
             return il;
         }
 
-        internal static ILGenerator ConvU8(this ILGenerator il)
+        public static ILGenerator ConvU8(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_U8);
             return il;
         }
 
-        internal static ILGenerator ConvI1(this ILGenerator il)
+        public static ILGenerator ConvI1(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_I1);
             return il;
         }
 
-        internal static ILGenerator ConvI2(this ILGenerator il)
+        public static ILGenerator ConvI2(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_I2);
             return il;
         }
 
-        internal static ILGenerator ConvI4(this ILGenerator il)
+        public static ILGenerator ConvI4(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_I4);
             return il;
         }
 
-        internal static ILGenerator ConvI8(this ILGenerator il)
+        public static ILGenerator ConvI8(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_I8);
             return il;
         }
 
-        internal static ILGenerator ConvR8(this ILGenerator il)
+        public static ILGenerator ConvR8(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_R8);
             return il;
         }
 
-        internal static ILGenerator ConvR4(this ILGenerator il)
+        public static ILGenerator ConvR4(this ILGenerator il)
         {
             il.Emit(OpCodes.Conv_R4);
             return il;
         }
 
-        internal static ILGenerator Tail(this ILGenerator il)
+        public static ILGenerator Tail(this ILGenerator il)
         {
             il.Emit(OpCodes.Tailcall);
             return il;
