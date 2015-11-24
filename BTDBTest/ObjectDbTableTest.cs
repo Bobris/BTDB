2c8fb24f25c4b2abc7630ff277fd6c50b9523af5
Bobris <Boris.Letocha@gmail.com>
2015-11-25 00:27:50 +0100

    Expand on Relations feature.

--- BTDBTest/ObjectDbTableTest.cs
+++ BTDBTest/ObjectDbTableTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using BTDB.KVDBLayer;
 using BTDB.ODBLayer;
 using NUnit.Framework;
@@ -44,6 +45,7 @@ namespace BTDBTest
             [PrimaryKey(2)]
             public Guid Id { get; set; }
             public string Name { get; set; }
+            [SecondaryKey("Age", IncludePrimaryKeyOrder = 1)]
             public uint Age { get; set; }
         }
 
@@ -66,6 +68,31 @@ namespace BTDBTest
                 creator(tr).Insert(new Person { TenantId = 1, Id = Guid.NewGuid(), Name = "Boris", Age = 39 });
                 tr.Commit();
             }
+        }
+
+        public interface IPersonTable
+        {
+            ulong TenantId { get; set; }
+            // Should Insert with different TenantId throw? Or it should set tenantId before writing?
+            // Insert will throw if already exists
+            void Insert(Person person);
+            // Upsert = Insert or Update - return true if inserted
+            bool Upsert(Person person);
+            // Update will throw if does not exist
+            void Update(Person person);
+            // It will throw if does not exists
+            Person FindById(Guid id);
+            // Will return null if not exists
+            Person FindByIdOrDefault(Guid id);
+            // Find by secondary key, it will throw if it find multiple Persons with that age
+            Person FindByAgeOrDefault(uint age);
+            IEnumerator<Person> FindByAge(uint age);  
+            // Returns true if removed, if returning void it does throw if does not exists
+            bool RemoveById(Guid id);
+            // fills all your iterating needs
+            IOrderedDictionaryEnumerator<Guid, Person> ListById(AdvancedEnumeratorParam<Guid> param);
+            IEnumerator<Person> GetEnumerator();
+            IOrderedDictionaryEnumerator<uint, Person> ListByAge(AdvancedEnumeratorParam<uint> param);
         }
 
     }
