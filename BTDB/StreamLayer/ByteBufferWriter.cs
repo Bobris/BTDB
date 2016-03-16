38c7e518915b7020a6e3cd0ccfd8e3329ff65377
Bobris <Boris.Letocha@gmail.com>
2016-03-16 23:52:58 +0100

    Simpler, Faster, Sorry.

--- BTDB/StreamLayer/ByteBufferWriter.cs
+++ BTDB/StreamLayer/ByteBufferWriter.cs
@@ -9,7 +9,7 @@ namespace BTDB.StreamLayer
 
         public ByteBufferWriter()
         {
-            Buf = new byte[4096];
+            Buf = new byte[32];
             End = Buf.Length;
         }
 
@@ -17,55 +17,28 @@ namespace BTDB.StreamLayer
         {
             get
             {
-                FinalFlushIfNeeded();
+                if (_result.Length == 0)
+                {
+                    _result = ByteBuffer.NewAsync(Buf, 0, Pos);
+                    Buf = null;
+                    Pos = 0;
+                    End = 1;
+                }
                 return _result;
             }
         }
 
         public override void FlushBuffer()
         {
-            JustFlushWithoutAllocNewBuffer();
-            if (Buf == null) Buf = new byte[4096];
+            var newLen = Math.Max((int)Math.Min((long)End * 2, 2147483591), 128);
+            if (newLen == End) throw new OutOfMemoryException();
+            Array.Resize(ref Buf, newLen);
             End = Buf.Length;
-            Pos = 0;
         }
 
         public override long GetCurrentPosition()
         {
-            return _result.Length + Pos;
-        }
-
-        void FinalFlushIfNeeded()
-        {
-            if (Pos != 0)
-            {
-                JustFlushWithoutAllocNewBuffer();
-                Pos = 0;
-                Buf = null;
-                End = 1;
-            }
-        }
-
-        void JustFlushWithoutAllocNewBuffer()
-        {
-            if (_result.Buffer == null || _result.Length == 0)
-            {
-                _result = ByteBuffer.NewAsync(Buf, 0, Pos);
-                Buf = null;
-            }
-            else
-            {
-                var origLength = _result.Length;
-                var newLength = origLength + Pos;
-                var b = _result.Buffer;
-                if (newLength > b.Length)
-                {
-                    _result = new ByteBuffer();
-                    Array.Resize(ref b, Math.Max(origLength*2, newLength));
-                }
-                Array.Copy(Buf, 0, b, origLength, Pos);
-                _result = ByteBuffer.NewAsync(b, 0, newLength);
-            }
+            return Pos;
         }
     }
 }
