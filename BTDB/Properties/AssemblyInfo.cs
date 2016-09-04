a047cd3bbdb354ac36195ce2adf18510a40c7634
Bobris <Boris.Letocha@gmail.com>
2016-09-04 23:21:42 +0200

    Bump version

--- BTDB/Properties/AssemblyInfo.cs
+++ BTDB/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
 
 [assembly: InternalsVisibleTo("BTDBTest")]
