1af2ab453c3c7a48ed61278d2b084d60aa1df5d3
Bobris <Boris.Letocha@gmail.com>
2017-03-09 07:11:16 +0100

    Update nugets and Release.

--- BTDB/Properties/AssemblyInfo.cs
+++ BTDB/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("9.3.0.0")]
-[assembly: AssemblyFileVersion("9.3.0.0")]
+[assembly: AssemblyVersion("9.3.1.0")]
+[assembly: AssemblyFileVersion("9.3.1.0")]
 
 [assembly: InternalsVisibleTo("BTDBTest")]
