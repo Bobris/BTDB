2869fc159897c235de094d3b97bd2952f573b4f5
Bobris <Boris.Letocha@gmail.com>
2017-06-27 22:44:38 +0200

    Release

--- BTDB/Properties/AssemblyInfo.cs
+++ BTDB/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("10.5.2.0")]
-[assembly: AssemblyFileVersion("10.5.2.0")]
+[assembly: AssemblyVersion("10.6.0.0")]
+[assembly: AssemblyFileVersion("10.6.0.0")]
 
 [assembly: InternalsVisibleTo("BTDBTest")]
