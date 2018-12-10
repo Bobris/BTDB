973f237cc1b54c0dd488e5f483e7bab9c94be213
Jan Vargovský <jan.vargovsky@gmail.com>
2018-12-10 14:47:16 +0100

    Added DtoChannel failing test.

--- BTDBTest/DTOChannelTest.cs
+++ BTDBTest/DTOChannelTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using BTDB.DtoChannel;
 using BTDB.EventStoreLayer;
 using BTDB.Service;
@@ -9,16 +10,14 @@ namespace BTDBTest
     public class DtoChannelTest : IDisposable
     {
         PipedTwoChannels _pipedTwoChannels;
-        ITypeSerializerMappingFactory _typeSerializers;
         IDtoChannel _first;
         IDtoChannel _second;
 
         public DtoChannelTest()
         {
             _pipedTwoChannels = new PipedTwoChannels();
-            _typeSerializers = new TypeSerializers();
-            _first = new DtoChannel(_pipedTwoChannels.First, _typeSerializers);
-            _second = new DtoChannel(_pipedTwoChannels.Second, _typeSerializers);
+            _first = new DtoChannel(_pipedTwoChannels.First, new TypeSerializers());
+            _second = new DtoChannel(_pipedTwoChannels.Second, new TypeSerializers());
         }
 
         public void Dispose()
@@ -53,6 +52,56 @@ namespace BTDBTest
             u2 = null;
             _first.Send(u1);
             Assert.True(u1.Equals((EventStoreTest.User)u2));
+        }
+
+        class IdentityUserV2
+        {
+            public IdentityUserMetadata Metadata { get; set; }
+        }
+
+        public class IdentityUserMetadata
+        {
+            public Dictionary<ulong, Dictionary<string, UserMetadataValue>> Application { get; set; }
+        }
+
+        public class UserMetadataValue
+        {
+            public string StringValue { get; set; }
+            public bool IsReadOnly { get; set; }
+        }
+
+        [Fact]
+        public void CanSendNestedDictionary()
+        {
+            var user = new IdentityUserV2
+            {
+                Metadata = new IdentityUserMetadata
+                {
+                    Application = new Dictionary<ulong, Dictionary<string, UserMetadataValue>>
+                    {
+                        [0] = new Dictionary<string, UserMetadataValue>
+                        {
+                            ["Foo"] = new UserMetadataValue
+                            {
+                                StringValue = "Bar",
+                            }
+                        }
+                    }
+                }
+            };
+            var firstReceived = new List<object>();
+            var secondReceived = new List<object>();
+            _first.OnReceive.Subscribe(firstReceived.Add);
+            _second.OnReceive.Subscribe(secondReceived.Add);
+
+            _first.Send(user);
+            var user1 = secondReceived[0];
+
+            _first.Send(user1); // fail
+            _second.Send(user1); // fail
+            var user2 = firstReceived[0];
+
+            _first.Send(user2);
         }
     }
 }
