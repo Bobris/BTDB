3bcf96ed459cb3df01078762e0449b3a59af4dc3
Boris Letocha <Boris.Letocha@gmail.com>
2019-05-15 00:59:39 +0200

    Add suffix getters and setters

--- BTDB/ARTLib/NodeUtils12.cs
+++ BTDB/ARTLib/NodeUtils12.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace BTDB.ARTLib
@@ -82,6 +83,36 @@ namespace BTDB.ARTLib
             return size;
         }
 
+        internal static unsafe (uint Size, IntPtr Ptr) GetSuffixSizeAndPtr(IntPtr suffixPtr, int i, int maxChildren)
+        {
+            if (suffixPtr == IntPtr.Zero)
+            {
+                return (0, IntPtr.Zero);
+            }
+            var ptr = suffixPtr;
+            var suffixOfs = *(ushort*)(ptr + i * 2);
+            var suffixSize = (uint)(*(ushort*)(ptr + i * 2 + 2) - suffixOfs);
+            ptr += 2 * maxChildren + 2 + suffixOfs;
+            return (suffixSize, ptr);
+        }
+
+        internal static unsafe void SetSuffix(IntPtr suffixPtr, int i, int maxChildren, (uint Size, IntPtr Ptr) src)
+        {
+            if (src.Ptr == IntPtr.Zero || src.Size == 0) return;
+            var ptr = suffixPtr;
+            var suffixOfs = *(ushort*)(ptr + i * 2);
+            var suffixOfs2 = *(ushort*)(ptr + i * 2 + 2);
+            Debug.Assert(suffixOfs == suffixOfs2);
+            var suffixOfs3 = *(ushort*)(ptr + 2 * maxChildren);
+            for (var j = i + 1; i <= maxChildren; j++)
+            {
+                *(ushort*)(ptr + j * 2) += (ushort)src.Size;
+            }
+            ptr += 2 * maxChildren + 2;
+            if (suffixOfs3 > suffixOfs2) ArtUtils.MoveMemory(ptr + suffixOfs2, ptr + (int)(suffixOfs2 + src.Size), suffixOfs3 - suffixOfs2);
+            ArtUtils.CopyMemory(src.Ptr, ptr + suffixOfs, (int)src.Size);
+        }
+
         internal static unsafe (uint Size, IntPtr Ptr) GetSuffixSizeAndPtr(IntPtr nodePtr, int i)
         {
             ref NodeHeader12 header = ref Ptr2NodeHeader(nodePtr);
@@ -99,6 +130,24 @@ namespace BTDB.ARTLib
             var suffixOfs = *(ushort*)(ptr + i * 2);
             var suffixSize = (uint)(*(ushort*)(ptr + i * 2 + 2) - suffixOfs);
             ptr += 2 * maxChildren + 2 + suffixOfs;
+            return (suffixSize, ptr);
+        }
+
+        internal static unsafe (uint, IntPtr) GetSuffixTotalSizeAndPtr(IntPtr nodePtr)
+        {
+            ref NodeHeader12 header = ref Ptr2NodeHeader(nodePtr);
+            if (!header._nodeType.HasFlag(NodeType12.HasSuffixes))
+            {
+                return (0u, IntPtr.Zero);
+            }
+            var (baseSize, maxChildren) = BaseSize(header._nodeType);
+            var prefixSize = (uint)header._keyPrefixLength;
+            var ptr = nodePtr + baseSize;
+            if (prefixSize == 0xffff)
+            {
+                ptr += sizeof(uint);
+            }
+            var suffixSize = *(ushort*)(ptr + maxChildren * 2);
             return (suffixSize, ptr);
         }
 
@@ -121,6 +170,64 @@ namespace BTDB.ARTLib
             ptr += (int)prefixSize;
             ptr = ArtUtils.AlignPtrUpInt32(ptr);
             return (12, ptr);
+        }
+
+        internal static unsafe (uint prefixSize, IntPtr prefixPtr, uint totalSuffixSize, IntPtr suffixPtr, uint valueSize, IntPtr valuePtr) GetAllSizeAndPtr(IntPtr nodePtr)
+        {
+            ref NodeHeader12 header = ref Ptr2NodeHeader(nodePtr);
+            var (baseSize, maxChildren) = BaseSize(header._nodeType);
+            var prefixSize = (uint)header._keyPrefixLength;
+            var ptr = nodePtr + baseSize;
+            if (prefixSize == 0xffff)
+            {
+                prefixSize = *(uint*)ptr;
+                ptr += sizeof(uint);
+            }
+            var suffixPtr = IntPtr.Zero;
+            var totalSuffixSize = 0u;
+            if (header._nodeType.HasFlag(NodeType12.HasSuffixes))
+            {
+                suffixPtr = ptr;
+                ptr += 2 * maxChildren;
+                totalSuffixSize = *(ushort*)ptr;
+                ptr += 2 + (int)totalSuffixSize;
+            }
+            var prefixPtr = ptr;
+            if (header._nodeType.HasFlag(NodeType12.IsLeaf))
+            {
+                ptr += (int)prefixSize;
+                ptr = ArtUtils.AlignPtrUpInt32(ptr);
+                return (prefixSize, prefixPtr, totalSuffixSize, suffixPtr, 12, ptr);
+            }
+            return (prefixSize, prefixPtr, totalSuffixSize, suffixPtr, 0, IntPtr.Zero);
+        }
+
+        internal static unsafe (uint prefixSize, IntPtr prefixPtr, IntPtr suffixPtr, uint valueSize, IntPtr valuePtr) GetAllSizeAndPtr(IntPtr nodePtr, uint totalSuffixSize)
+        {
+            ref NodeHeader12 header = ref Ptr2NodeHeader(nodePtr);
+            var (baseSize, maxChildren) = BaseSize(header._nodeType);
+            var prefixSize = (uint)header._keyPrefixLength;
+            var ptr = nodePtr + baseSize;
+            if (prefixSize == 0xffff)
+            {
+                prefixSize = *(uint*)ptr;
+                ptr += sizeof(uint);
+            }
+            var suffixPtr = IntPtr.Zero;
+            if (header._nodeType.HasFlag(NodeType12.HasSuffixes))
+            {
+                suffixPtr = ptr;
+                ptr += 2 * maxChildren;
+                ptr += 2 + (int)totalSuffixSize;
+            }
+            var prefixPtr = ptr;
+            if (header._nodeType.HasFlag(NodeType12.IsLeaf))
+            {
+                ptr += (int)prefixSize;
+                ptr = ArtUtils.AlignPtrUpInt32(ptr);
+                return (prefixSize, prefixPtr, suffixPtr, 12, ptr);
+            }
+            return (prefixSize, prefixPtr, suffixPtr, 0, IntPtr.Zero);
         }
 
         internal static IntPtr PtrInNode(IntPtr node, int posInNode)
