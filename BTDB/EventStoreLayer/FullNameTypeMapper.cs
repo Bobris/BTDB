808770edc1c5c673bc99b1af703eb06b0cc84276
Jan Vargovský <jan.vargovsky@gmail.com>
2019-06-08 14:13:12 +0200

    FullNameTypeMapper.ToName reworked + tests.

--- BTDB/EventStoreLayer/FullNameTypeMapper.cs
+++ BTDB/EventStoreLayer/FullNameTypeMapper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace BTDB.EventStoreLayer
 {
@@ -6,7 +7,39 @@ namespace BTDB.EventStoreLayer
     {
         public string ToName(Type type)
         {
-            return type.FullName;
+            if (!type.IsGenericType)
+                return type.FullName;
+
+            var sb = new StringBuilder();
+            ToName(type, sb);
+            return sb.ToString();
+        }
+
+        void ToName(Type type, StringBuilder sb)
+        {
+            if (type.IsGenericType)
+            {
+                sb
+                    .Append(type.Namespace)
+                    .Append('.')
+                    .Append(type.Name, 0, type.Name.IndexOf('`'))
+                    .Append('<');
+
+                var args = type.GetGenericArguments();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (i != 0)
+                        sb.Append(',');
+                    ToName(args[i], sb);
+                }
+
+                sb.Append('>');
+            }
+            else
+            {
+                sb.Append(type.FullName);
+            }
+
         }
 
         public Type ToType(string name)
---
+++ BTDBTest/FullNameTypeMapperTest.cs
@@ -0,0 +1,49 @@
+﻿using BTDB.EventStoreLayer;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BTDBTest
+{
+    public class FullNameTypeMapperTest
+    {
+        readonly FullNameTypeMapper _fullNameTypeMapper;
+
+        public FullNameTypeMapperTest()
+        {
+            _fullNameTypeMapper = new FullNameTypeMapper();
+        }
+
+        [Theory]
+        [MemberData(nameof(Mappings))]
+        public void ToNameTest(Type type, string name)
+        {
+            var actualName = _fullNameTypeMapper.ToName(type);
+            Assert.Equal(name, actualName);
+        }
+
+        [Theory]
+        [MemberData(nameof(Mappings))]
+        public void ToTypeTest(Type type, string name)
+        {
+            var actualType = _fullNameTypeMapper.ToType(name);
+            Assert.Equal(type, actualType);
+        }
+
+        public static List<object[]> Mappings => new List<object[]>
+        {
+            new object[] {typeof(int), "System.Int32"},
+            new object[] {typeof(IEnumerable<int>), "System.Collections.Generic.IEnumerable<System.Int32>"},
+            new object[] {typeof(IDictionary<int, string>), "System.Collections.Generic.IDictionary<System.Int32,System.String>"},
+            new object[] {typeof(NestedClass), "BTDBTest.FullNameTypeMapperTest+NestedClass"},
+            new object[] {typeof(NestedClass.NestedClass2), "BTDBTest.FullNameTypeMapperTest+NestedClass+NestedClass2"}
+        };
+
+        class NestedClass
+        {
+            internal class NestedClass2
+            {
+            }
+        }
+    }
+}
