6dbbf76c80076824d0734acf365afa896accc64a
Jan Vícha <j.vicha@quadient.com>
2020-03-26 13:37:44 +0100

    added ToString override for EncryptedString

--- BTDB/Encrypted/EncryptedString.cs
+++ BTDB/Encrypted/EncryptedString.cs
@@ -5,5 +5,6 @@ namespace BTDB.Encrypted
         public static implicit operator EncryptedString(string? secret) => new EncryptedString {Secret = secret};
         public static implicit operator string?(EncryptedString secret) => secret.Secret;
         public string? Secret;
+        public override string ToString() => Secret;
     }
 }
